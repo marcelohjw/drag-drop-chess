fec459ab61e355dff6c76930144b5658fda7a2b8
Marcelo Junior <marcelohjw@gmail.com>
2023-03-18 15:15:25 -0300

    Moving pieces with js

---
+++ index..js
@@ -0,0 +1,25 @@
+const king = document.querySelector('.chess-piece');
+const squares = document.querySelectorAll('.square');
+
+
+king.addEventListener('dragstart', dragStart);
+
+squares.forEach(square => {
+    square.addEventListener('dragover', dragOver);
+    square.addEventListener('drop', dragDrop);
+});
+
+let beingDragged;
+
+function dragStart(e) {
+    beingDragged = e.target;
+}
+
+function dragOver(e) {
+    e.preventDefault();
+}
+
+function dragDrop(e) {
+    e.target.append(beingDragged);
+}
+
